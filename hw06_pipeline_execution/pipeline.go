72040303a9629a935c1d7da292be1b2202b4cfbc
mxmntv <mxmmmntv@gmail.com>
2022-10-24 22:01:51 +0300

    HW 6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,30 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+
+	worker := func(d, in In) Out {
+		temp := make(Bi)
+		go func() {
+			defer close(temp)
+			for {
+				select {
+				case data, ok := <-in:
+					if !ok {
+						return
+					}
+					temp <- data
+				case <-d:
+					return
+				}
+			}
+		}()
+		return temp
+	}
+
+	for _, s := range stages {
+		out = worker(done, s(out))
+	}
+
+	return out
 }
