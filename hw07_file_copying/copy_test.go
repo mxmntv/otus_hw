99a5ac9a5dcac3e345170dcce94b7dd2be6f3bea
mxmntv <mxmmmntv@gmail.com>
2022-11-20 19:56:27 +0300

    HW7 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,76 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	name, err := os.MkdirTemp("./", "copied")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t.Run("simple case", func(t *testing.T) {
+		dest := "./" + name + "/cinput.txt"
+		Copy("./testdata/input.txt", dest, 0, 0)
+		src, err := os.Open(dest)
+		if err != nil {
+			log.Fatal(err)
+		}
+		i, _ := GetLen(src)
+		require.Equal(t, 6617, i)
+	})
+
+	t.Run("offset 0, limit 10 case", func(t *testing.T) {
+		dest := "./" + name + "/cout_offset0_limit10.txt"
+		Copy("./testdata/out_offset0_limit10.txt", dest, 0, 10)
+		src, err := os.Open(dest)
+		if err != nil {
+			log.Fatal(err)
+		}
+		i, _ := GetLen(src)
+		require.Equal(t, 10, i)
+	})
+
+	t.Run("offset 0, limit 1000 case", func(t *testing.T) {
+		dest := "./" + name + "/cout_offset0_limit1000.txt"
+		Copy("./testdata/out_offset0_limit1000.txt", dest, 0, 1000)
+		src, err := os.Open(dest)
+		if err != nil {
+			log.Fatal(err)
+		}
+		i, _ := GetLen(src)
+		require.Equal(t, 1000, i)
+	})
+
+	t.Run("offset 0, limit 10000 case", func(t *testing.T) {
+		dest := "./" + name + "/cout_offset0_limit10000.txt"
+		Copy("./testdata/out_offset0_limit10000.txt", dest, 0, 10000)
+		src, err := os.Open(dest)
+		if err != nil {
+			log.Fatal(err)
+		}
+		i, _ := GetLen(src)
+		fmt.Println(i)
+		require.Equal(t, 6617, i)
+	})
+
+	t.Run("offset 100, limit 1000 case", func(t *testing.T) {
+		dest := "./" + name + "/cout_offset100_limit1000.txt"
+		Copy("./testdata/out_offset100_limit1000.txt", dest, 100, 1000)
+		src, err := os.Open(dest)
+		if err != nil {
+			log.Fatal(err)
+		}
+		i, _ := GetLen(src)
+		fmt.Println(i)
+		require.Equal(t, 900, i)
+	})
+
+	defer os.RemoveAll(name)
 }
