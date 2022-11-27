3bb0bc7ba9480974d85b27c86e1f1a2ea6afe9d6
mxmntv <mxmmmntv@gmail.com>
2022-11-27 22:26:40 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bytes"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,30 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	env := make(Environment)
+	for _, f := range files {
+		d, err := os.ReadFile(path.Join(dir, f.Name()))
+		if err != nil {
+			return nil, err
+		}
+		fi, err := f.Info()
+		if err != nil {
+			return nil, err
+		}
+		env[strings.TrimRight(f.Name(), "=")] = EnvValue{
+			clearStr(string(d)),
+			fi.Size() == 0,
+		}
+	}
+	return env, nil
+}
+
+func clearStr(s string) string {
+	ms := strings.Split(s, "\n")[0]
+	ms = string(bytes.ReplaceAll([]byte(ms), []byte{0x00}, []byte("\n")))
+	return strings.TrimRight(ms, " \t")
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	dir := os.Args[1]
+	comand := os.Args[2:]
+	v, e := ReadDir(dir)
+	if e != nil {
+		os.Exit(1)
+	}
+	os.Exit(RunCmd(comand, v))
 }
+
+// RUN go build && ./hw08_envdir_tool ./testdata/env /bin/bash ./testdata/echo.sh arg1=1 arg2=2
